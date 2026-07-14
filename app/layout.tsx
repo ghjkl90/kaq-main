46bb9cd263e83d8b74e30f14349208e1b029c5c0
ghjkl90 <jlu4688@gmail.com>
2026-07-14 13:29:36 +0900

    header 문법 오류 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <Header />
+        <Header onOpenContact={() => {}} />
         <div className="flex-1">
           {children}
         </div>
